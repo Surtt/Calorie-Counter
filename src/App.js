ea07e164a7d77b18e6be38971ba4945c851e7b62
Alexander Sergeev <aleksanteri.sergeev@gmail.com>
2021-06-15 23:10:46 +0300

    added formik

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
+import Title from './components/Title';
+import Form from './components/Form';
+import Result from './components/Result';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <article className="counter">
+      <Title />
+      <Form />
+      <Result />
+    </article>
   );
 }
 
---
+++ src/components/Title.jsx
@@ -0,0 +1,7 @@
+const Title = () => (
+  <h1 className="counter__heading heading-main">
+    Счётчик калорий
+  </h1>
+);
+
+export default Title;
