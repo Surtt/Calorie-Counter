838f31f5d034822c317da115517b75f0f0018b0d
Alexander Sergeev <aleksanteri.sergeev@gmail.com>
2021-06-22 12:14:24 +0300

    updated form

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <article className="counter">
       <Title />
       <Form />
-      <Result />
+      {/* <Result /> */}
     </article>
   );
 }
--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,13 +1,28 @@
-const Result = () => {
+import React from 'react';
+
+const Result = ({ show, values }) => {
+  console.log(values);
+  let result = 0;
+  if (values.gender === 'female') {
+    result = (10 * +values.weight) + (6.25 * +values.height) - (5 * +values.age) - 161;
+    console.log(result * 1.2);
+    // return result * 1.2;
+  }
+  // const result = Object.entries(values).reduce((acc, [key, value]) => {
+  //   if (values.gender === 'female') {
+  //       console.log('ff');
+  //     }
+  // }, 0);
+  const cs = show ? 'counter__result' : 'counter__result counter__result--hidden';
   return (
-    <section className="counter__result counter__result--hidden">
+    <section className={cs}>
       <h2 className="heading">
         Ваша норма калорий
       </h2>
       <ul className="counter__result-list">
         <li className="counter__result-item">
           <h3>
-            <span id="calories-norm">3 800</span> ккал
+            <span id="calories-norm">{result * 1.2}</span> ккал
           </h3>
           <p>
             поддержание веса
